ceab697448b0d0cec16ba3f5f582e9eaba37ffc4
Rakesh Dhamathotti <153016869+RakeshDhamathotti@users.noreply.github.com>
2026-06-17 12:06:32 +0530

    Delete ApiDelpoyment/build directory

--- ApiDelpoyment/build/src/main/java/Api/build/ExceptionHandling/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package Api.build.ExceptionHandling;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-    
-}
